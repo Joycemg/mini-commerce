a3e52ef26a2e462413acc6e25bdd20d158b2503c
Marce /Joyce <Marce.giamp@gmail.com>
2021-12-19 11:09:57 -0300

    Add the 'Header' component and its style.

---
+++ src/components/header_app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import "./assets/css/cssStyle.css";
+import "../assets/css/cssStyle.css"
+export const HeaderApp = () => (
+    <div className='Header'>
+        <h1>Mini-Ecommerce</h1>
+    </div>
+);
+
